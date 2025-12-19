07b6bf121b08775e525c124408c4e6c190ddd2e0
and0402 <adefalsjr@gmail.com>
2025-12-19 17:12:27 +0700

    menambah merch shop & kolaborasi

--- resoluasi.py
+++ resoluasi.py
@@ -2,3 +2,5 @@ SAZUKE RESOLUSI
 - konsisten dalam membuat project harian
 - sedang mempelajari tentang dunia cybersecurity
 - bisa menguasai semua bahasa pemoraman terutama python 
+- Membuat Platfrom untuk shop
+- Kerja sama orang yang lebih hebat
